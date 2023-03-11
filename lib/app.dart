43822a1a9020598ec7962d4c887bfe69ef59e025
Flutter Boy <85279360+flutterboy20@users.noreply.github.com>
2023-03-11 21:11:27 +0530

    Updated app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -10,7 +10,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dictionary App using Blco',
+      title: 'Dictionary App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
